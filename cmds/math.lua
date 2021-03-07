0f0e575a33562794771e5b73b9b1faa3274f1899
CoolingTool <coolingtool@outlook.com>
2021-03-07 08:45:29 -0600

    math improvements

--- cmds/math.lua
+++ cmds/math.lua
@@ -2,15 +2,19 @@ local calc = commands 'math' '[equation]'
 calc.help = 'runs equation using lua'
 calc.alias = {'calc', 'calculator'}
 
-string.rep = nil
+local blacklist = {'"', "'", "[[", "]]", "{", "}", "function"}
 
 function calc:run(param)
     if not param then return nil, 'equation needed' end
-    if param:find("function") then return nil, 'no' end
+    local blacklisted = false
+    for i, b in pairs(blacklist) do blacklisted = param:find(b, 1, true)
+    if blacklisted then break end end
+    if blacklisted then return nil, 'no' end
+
+    local param = param:find("^9%s*+%s*10$") and '21' or param
 
     local sandbox = setmetatable({
-        math = math, bit = bit, F = F,
-        utf8 = utf8, string = string,
+        math = math, bit = bit
     },{__index = function(t, i)
         local ret = math[i] or bit[i]
         if not ret then
@@ -31,10 +35,10 @@ function calc:run(param)
             return nil, "no result"
         else
             for i = 1, ret.n do
-                ret[i] = pp.dump(ret[i], nil, true)
+                ret[i] = tostring(ret[i])
             end
 
-            return table.concat(ret, ', '), {safe = true}
+            return help.code(table.concat(ret, ', '), 'py'), {safe = true}
         end
     end
 
