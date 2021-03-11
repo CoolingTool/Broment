598297230016eb21ab5bedeb199be100b2966aa7
CoolingTool <coolingtool@outlook.com>
2021-03-11 06:10:21 -0600

    add ss command

---
+++ cmds/ss.lua
@@ -0,0 +1,13 @@
+local ss = commands 'ss' '[url]'
+ss.help = 'screenshot a site for when notsobot is down'
+ss.alias = 'screenshot'
+
+function ss:run(param, perms)
+    if not param then return nil, 'site needed' end
+    if not perms.bot:has'attachFiles' then return nil, 'i need image perms' end
+
+    local wait = self:reply('please wait...')
+    self.channel:broadcastTyping()
+
+    return {file = {'ss.png', help.fapi('screenshot', {text = param})}}, {remove = wait}
+end
