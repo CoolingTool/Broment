2fb7a83a02a0b77424fa6b6781fc9f7ebf451756
CoolingTool <CoolingTool@users.noreply.github.com>
2021-10-10 23:16:17 -0500

    loop through messages from bottom to top

--- cmds/sticker.lua
+++ cmds/sticker.lua
@@ -8,7 +8,7 @@ sticker.alias = {
 sticker.help = 'gets sticker and converts it to gif'
 
 local format = {
-    --type 1 is just a normal png but link is unknown
+    [1] = {'https://media.discordapp.net/stickers/','.png', nil},
     [2] = {'https://media.discordapp.net/stickers/','.png', help.apng2gif},
     [3] = {'https://discord.com/stickers/','.json', help.lottie2gif}
 }
@@ -18,14 +18,13 @@ function sticker:run(param, perms)
     if not perms.bot:has'attachFiles' then return nil, 'i need image perms' end
     
     local data = help.APIget(message.channel, message.id)
-    local stickers = data.stickers
+    local stickers = data.stickers or data.sticker_items -- thanks for being inconsistant discord
 
     if not stickers then 
-        local messages = API:getChannelMessages(self.channel.id)
+        local messages = table.reversed(API:getChannelMessages(self.channel.id))
         for i, m in pairs(messages) do
-            if m.stickers then
-                stickers = m.stickers
-                break
+            if (m.stickers or m.sticker_items) then
+                stickers = m.stickers or m.sticker_items
             end
         end
     end
@@ -36,16 +35,21 @@ function sticker:run(param, perms)
         local sticker = select(2, next(stickers))
         local known = format[sticker.format_type]
         if known then
-            local link = known[1]..help.concat"/"(sticker.id, sticker.asset)..known[2]
+            local link = known[1]..sticker.id..known[2]
             
             local succ, res, source = pcall(http.request, 'GET', link)
 
-            local result = known[3](source)
-            
-            return {
-                content = '<'..link..'>',
-                file = {'%s.gif'%{sticker.name}, result}
-            }, {remove = wait}
+            local action = known[3]
+            if not action then -- if action is nil discord can probably display it on its own
+                return link..' _ _', {remove = wait}
+            else
+                local result = action(source)
+                
+                return {
+                    content = '<'..link..'>',
+                    file = {'%s.gif'%{sticker.name}, result}
+                }, {remove = wait}
+            end
         else
             return nil, 'i don\'t know how to get the link of this sticker'
         end
